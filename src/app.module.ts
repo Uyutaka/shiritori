2225c00db62cbca9bea86a38744a6d545f16340a
Yutaka <yutaka.urakami@colorado.edu>
2022-03-27 15:23:31 +0900

    run nest g controller games

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { GamesController } from './games/games.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, GamesController],
   providers: [AppService],
 })
 export class AppModule {}
